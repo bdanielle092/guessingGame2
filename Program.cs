cfdbd432d716b59ec0de7465c00ffe2aeb29ef07
Faith Magras <bdanielle092@gmail.com>
2020-11-16 11:47:32 -0600

    added difficulty level

--- Program.cs
+++ Program.cs
@@ -8,15 +8,44 @@ namespace guessingGame2
         {
             // var called secretNumber which is a number between 1 - 100
            int secretNumber = new Random().Next(1, 101);
+           int difficultyMode = 0;
            int userTries = 0;
+
+         while (difficultyMode == 0)
+         {
+             Console.WriteLine("Select a difficulty:");
+             Console.WriteLine("1) Easy - eight guesses");
+             Console.WriteLine("2) Medium - six guesses");
+             Console.WriteLine("3) Hard - four guesses");
+
+             difficultyMode = int.Parse(Console.ReadLine());
+             switch (difficultyMode)
+             {
+                 case 1:
+                 userTries = 8;
+                 break;
+                 case 2:
+                 userTries = 6;
+                 break;
+                 case 3: 
+                 userTries = 4;
+                 break;
+                 default:
+                 userTries = 8;
+                 break;
+             }
+
+         }
+
            //prompt the user to guess the secret number
             Console.WriteLine("What's the secret Number ?");
             //display  a prompt to guess
            int guess = int.Parse(Console.ReadLine());
-            // useing a while loop to guess the secretNumber and a if/else to check how many times a user has guessed
-            while(guess != secretNumber && userTries < 3){
+            // useing a while loop to guess the secretNumber and the amount to time a user has guesseds
+            while(guess != secretNumber && userTries != 1){
 
                 Console.WriteLine("guess again ?");
+                //if else statement for guessing to low or high
                 if(guess < secretNumber){
                     Console.WriteLine("");
                     Console.WriteLine("You guess is to low");
@@ -24,15 +53,15 @@ namespace guessingGame2
                     Console.WriteLine("");
                     Console.WriteLine("You guess is to high");
                 }
-                 userTries++;
+                 userTries--;
                  // tells the user the amount of time user has guessed 
                 Console.WriteLine($"You have tired {userTries} times.");
+                //allow the user to guess
                 guess = int.Parse(Console.ReadLine());
                
-                
-              
             }
-              if(userTries < 3 || guess == secretNumber){
+            //if else statement on weather they got it right or to wrong
+              if(userTries != 1 || guess == secretNumber){
                   Console.WriteLine("Awesome You got it right!");
 
               }else{

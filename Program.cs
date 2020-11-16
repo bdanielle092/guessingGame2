c85a6e619339bd2d2a99402d5b5e9615ad55586b
Faith Magras <bdanielle092@gmail.com>
2020-11-16 15:07:05 -0600

    added a few comments

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace guessingGame2
     {
         static void Main(string[] args)
         {
-            // we are assigning the variables. int in the type. secrestNumber is the variable and we are assigning class its Random().Next()
-            // secretNumber  = a random number between 1 - 100
+            // we are assigning the variables. int in the type. secrestNumber is the variable and we are assigning it Random().Next(1, 100)
+            // secretNumber  = a random number between 1 - 100 
+            //.Next Returns a non-negative random integer.
            int secretNumber = new Random().Next(1, 101);
-           // difficulty level = 0
            int difficultyLevel = 0;
-           // userTries = 0 
            int userTries = 0;
-        // used a switch to have user pick a difficulty level 
+        // used a switch case to have user pick a difficulty level 
+        //while difficultyLevel == 0 then they can pick a level
          while (difficultyLevel == 0)
          {
              Console.WriteLine("Select a difficulty:");
@@ -46,30 +46,31 @@ namespace guessingGame2
          }
 
            //prompt the user to guess the secret number
-            Console.WriteLine("What's the secret Number ?");
-            //display  a prompt to guess
-           int guess = int.Parse(Console.ReadLine());
-            // using a while loop to guess the secretNumber and the amount to time a user has guesseds
-            while(guess != secretNumber && userTries != 1){
+            Console.WriteLine("Guess the secret Number ?");
+            //display a prompt to the user to guess a number 
+           int usersGuess = int.Parse(Console.ReadLine());
+            // using a while loop to guess the secretNumber and the amount to time a user has guesses
+            while(usersGuess != secretNumber && userTries != 1){
 
-                Console.WriteLine("guess again ?");
+               
                 //if else statement for guessing if the number is  to low or to high
-                if(guess < secretNumber){
+                if(usersGuess < secretNumber){
                     Console.WriteLine("");
                     Console.WriteLine("You guess is to low");
                 }else{
                     Console.WriteLine("");
                     Console.WriteLine("You guess is to high");
                 }
+                //subtracts the number of times a user has tried
                  userTries--;
-                 // tells the user the amount of time user has guessed 
-                Console.WriteLine($"You have tired {userTries} times.");
+                 // tells the user the amount of times they have left to guess
+                Console.WriteLine($"You have {userTries} guesses left.");
                 //allow the user to guess
-                guess = int.Parse(Console.ReadLine());
+                usersGuess = int.Parse(Console.ReadLine());
                
             }
             //if else statement on weather they got it right or to wrong
-              if(userTries != 1 || guess == secretNumber){
+              if(userTries != 1 || usersGuess == secretNumber){
                   Console.WriteLine("Awesome You got it right!");
 
               }else{

34353cc112c2a6ca57de3b8a224980f59fa89f81
Faith Magras <bdanielle092@gmail.com>
2020-11-16 10:27:13 -0600

    user get 4 guesses

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ namespace guessingGame2
         {
             
            int secretNumber = 43;
+           int Usertry = 1;
             Console.WriteLine("What's the secret Number ?");
            int guess = int.Parse(Console.ReadLine());
-            // Console.WriteLine(guess);
-            while(guess != secretNumber ){
+            
+            while(guess != secretNumber && Usertry < 4){
                 Console.WriteLine("guess again ?");
                 guess = int.Parse(Console.ReadLine());
+                Usertry++;
             }
                 Console.WriteLine("You got it right!");
             

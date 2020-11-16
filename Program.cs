37b48f80d333e0bcc40f70ccf7ff3158a0599566
Faith Magras <bdanielle092@gmail.com>
2020-11-16 10:04:27 -0600

    displays users guess

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace guessingGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            Console.WriteLine("What's the secret Number ?");
+            string guess = Console.ReadLine();
+            Console.WriteLine(guess);
+            
+        }
+    }
+}
